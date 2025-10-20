7988e1b39d0717e94105242cd68f37a9b6d8bff0
JayKadi <jaykadi42@gmail.com>
2025-10-20 17:55:00 +0300

    Enables login in mobile phone

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -37,7 +37,7 @@ function LoginPage() {
   const token = credentialResponse.credential;
   console.log("Google token:", token);
 
-  fetch("http://localhost:8000/api/google-login/", {
+  fetch("https://expense-tracker-api-nw3h.onrender.com/api/google-login/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
